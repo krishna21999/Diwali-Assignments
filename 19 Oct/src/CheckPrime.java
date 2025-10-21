ba130169de11846f5ded99fae51a3548a1ad7f19
krishna ingole <ingolek21999@gmail.com>
2025-10-21 09:08:56 +0530

    Initial Commit

---
+++ 19 Oct/src/CheckPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int n = sc.nextInt();
+		
+		boolean isPrime = true;
+		
+		if(n<=1) {
+			isPrime = false;
+		}else {
+			for(int i =2; i<n; i++) {
+				if(n%i==0) {
+					isPrime = false;
+				}
+			}
+		}
+		if(isPrime) {
+			System.out.println("Number is Prime");
+		}else {
+			System.out.println("Number is not prime");
+		}
+		
+	}
+
+}
---
+++ 19 Oct/src/FibonacciSeries.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci series: ");
+        if (n >= 1) System.out.print(a + " ");
+        if (n >= 2) System.out.print(b + " ");
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
