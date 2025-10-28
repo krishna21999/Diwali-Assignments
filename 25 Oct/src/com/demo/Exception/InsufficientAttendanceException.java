3a3b2635772161cb36be06c5e61a9a3d35563f2d
krishna21999 <122163520+krishna21999@users.noreply.github.com>
2025-10-28 14:49:54 +0530

    Delete 25 Oct directory

--- 25 Oct/src/com/demo/Exception/InsufficientAttendanceException.java
+++
@@ -1,8 +0,0 @@
-package com.demo.Exception;
-
-public class InsufficientAttendanceException extends Exception {
-
-	public InsufficientAttendanceException(String message) {
-        super(message);
-    }
-}
